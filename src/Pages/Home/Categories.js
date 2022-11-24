30ebda6d6dbcbb3b62e911312efe9178b88e29f0
Tahmina <tahminakhatun5447@gmail.com>
2022-11-24 12:10:59 +0600

    install react query and load categories data

---
+++ src/Pages/Home/Categories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import bed from '../../images/bed2.jpg'
+
+const Categories = () => {
+    
+    return (
+        <div className='my-16'>
+            <div>
+                <h1 className='text-center text-primary text-2xl font-bold'>Categories</h1>
+            </div>
+            <div className='mt-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Categories from './Categories';
 import HomeSlider from './HomeSlider';
 import WhatWeOffer from './WhatWeOffer';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <HomeSlider></HomeSlider>
+            <Categories></Categories>
             <WhatWeOffer></WhatWeOffer>
         </div>
     );
